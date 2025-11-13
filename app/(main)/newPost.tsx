4289d7005c6d26a20c8057bf945d4b79dfb25473
Ornerd <ukoorbytal@gmail.com>
2025-11-13 11:43:59 +0100

    new post setup

--- app/(main)/newPost.tsx
+++ app/(main)/newPost.tsx
@@ -1,14 +1,37 @@
+import Header from '@/components/Header'
+import ScreenWrapper from '@/components/ScreenWrapper'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, View } from 'react-native'
 
 const NewPost = () => {
   return (
-    <View>
-      <Text>NewPost</Text>
-    </View>
+    <ScreenWrapper bg='white'> 
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+      >
+        
+          <View style={styles.container}>
+              <Header
+                  title='Add new post'
+                  showBackButton={true}
+              />
+
+              <ScrollView style={{flex: 1}}>
+                
+              </ScrollView>
+          </View>
+        
+      </KeyboardAvoidingView>
+    </ScreenWrapper>
   )
 }
 
 export default NewPost
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+  }
+})
