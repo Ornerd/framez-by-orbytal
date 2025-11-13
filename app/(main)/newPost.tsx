a54012d09891a7a15686482a6075521ec0e6d746
Ornerd <ukoorbytal@gmail.com>
2025-11-13 12:07:54 +0100

    richtext editor installation

--- app/(main)/newPost.tsx
+++ app/(main)/newPost.tsx
@@ -1,15 +1,17 @@
 import AvatarDp from '@/components/AvatarDp'
 import Header from '@/components/Header'
+import RichTextEditor from '@/components/RichTextEditor'
 import ScreenWrapper from '@/components/ScreenWrapper'
 import { theme } from '@/constants/theme'
 import { useAuth } from '@/contexts/AuthContext'
 import { heigthPercentage } from '@/helpers/common'
-import React from 'react'
+import React, { useRef } from 'react'
 import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 const NewPost = () => {
 
   const {user} = useAuth();
+  const bodyRef = useRef('')
 
   return (
     <ScreenWrapper bg='white'> 
@@ -38,7 +40,14 @@ const NewPost = () => {
                           user && user.name
                         }
                     </Text>
+                    <Text style={styles.publicText}>
+                        public
+                    </Text>
                   </View>
+                </View>
+
+                <View style={styles.textEditor}>
+                        <RichTextEditor/>
                 </View>
 
               </ScrollView>
@@ -55,10 +64,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 24,
+    gap: 20
   }, 
   header: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: '12'
+    gap: '12',
+  },
+  username: {
+    fontSize: 18,
+    fontWeight: theme.fonts.semiBold
+  },
+  publicText: {
+
+  },
+  textEditor: {
+    marginTop: 20
   }
 })
---
+++ components/RichTextEditor.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RichTextEditor = () => {
+  return (
+    <View>
+      <Text>RichTextEditor</Text>
+    </View>
+  )
+}
+
+export default RichTextEditor
+
+const styles = StyleSheet.create({})
